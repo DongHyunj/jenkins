580cd9944337f26ad6398c53aa77d8964f2051a2
jdh <wjdehd555@gmail.com>
2026-04-06 11:22:57 +0900

    Feat: cicd 테스트

--- src/main/java/org/example/jenkins/CicdController.java
+++ src/main/java/org/example/jenkins/CicdController.java
@@ -1,4 +1,16 @@
 package org.example.jenkins;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/cicd")
 public class CicdController {
+
+    @GetMapping("/test")
+    public ResponseEntity test() {
+        return ResponseEntity.ok("성공");
+    }
 }
