34cebed25ed546f72f72652b6796d24f2a62c075
jdh <wjdehd555@gmail.com>
2026-04-06 15:07:24 +0900

    Test: v7

--- src/main/java/org/example/jenkins/CicdController.java
+++ src/main/java/org/example/jenkins/CicdController.java
@@ -1,9 +1,11 @@
 package org.example.jenkins;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/cicd")
 public class CicdController {
